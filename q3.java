9c86781b021314cbf47eb25d94a01bdc0df9b843
KRISHNA2739 <68938528+KRISHNA2739@users.noreply.github.com>
2020-10-21 10:23:55 +0530

    Add files via upload

---
+++ q3.java
@@ -0,0 +1,38 @@
+package day1;
+import java.util.Scanner;
+public class q3 {
+	
+
+	
+		public static void main(String[] args) {
+			int n;
+			Scanner sc= new Scanner(System.in);
+			System.out.println("Enter the number of students:");
+			n= sc.nextInt();
+			if(n>0)
+			{
+				int m;
+				double s=0;
+			
+			int[] g = new int[n];
+			for(int i=0;i<n;i++)
+			{
+				System.out.println("enter the grade for student"+i+":");
+				m=sc.nextInt();
+				if(m>100||m<0) {
+					System.out.println("please enter valid marks");
+					i--;
+					continue;
+				}
+				g[i]=m;
+				
+			}
+			
+			for(int i=0;i<n;i++) {
+				s=s+g[i];
+			}
+			System.out.println("the average is: "+s/n);
+		}
+		
+	}
+}
