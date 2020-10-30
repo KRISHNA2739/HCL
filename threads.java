d7b14c68946a79277692245c5bfdf4fbc4877f41
KRISHNA2739 <68938528+KRISHNA2739@users.noreply.github.com>
2020-10-30 07:27:34 +0530

    Add files via upload

---
+++ threads.java
@@ -0,0 +1,54 @@
+package practice;
+
+
+class Cal implements Runnable
+{
+  
+
+private int i=0;
+   
+
+
+public void run ()
+  {
+    for(int i=0;i<=100;i++)
+	  System.out.println(Thread.currentThread().getName()+" "+i);
+  
+} 
+ 
+
+
+}
+
+
+public class threads
+{
+  
+ 
+ 
+public static void main (String[]args) throws InterruptedException
+  {
+    
+
+
+
+
+Cal cal =new Cal();
+    
+Thread thread1 = new Thread (cal);
+Thread thread2 = new Thread (cal);
+Thread thread3 = new Thread (cal);
+    
+thread1.start ();
+thread2.start ();
+thread3.start ();
+    
+thread1.join ();
+thread2.join ();
+thread3.join ();
+    
+
+
+} 
+
+} 
